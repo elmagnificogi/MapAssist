3c7e8abc8e99d0b60af24525a6ec5f28bfbdf081
elmagnifico <elmagnificogg@gmail.com>
2022-05-13 17:54:16 +0800

    change get dc info method

---
+++ Helpers/Serializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapAssist.Helpers
+{
+    public class Serializer
+    {
+        /// 将对象序列化为json文件
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="t">实例</param>
+        /// <param name="path">存放路径</param>
+        public static void ObjectToJson<T>(T t, string path) where T : class
+        {
+            var formatter = new DataContractJsonSerializer(typeof(T));
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                formatter.WriteObject(stream, t);
+            }
+        }
+
+        /// <summary>
+        /// 将对象序列化为json字符串
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="t">实例</param>
+        /// <returns>json字符串</returns>
+        public static string ObjectToJson<T>(T t) where T : class
+        {
+            var formatter = new DataContractJsonSerializer(typeof(T));
+            using (var stream = new MemoryStream())
+            {
+                formatter.WriteObject(stream, t);
+                var result = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// json字符串转成对象
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="json">json格式字符串</param>
+        /// <returns>对象</returns>
+        public static T JsonToObject<T>(string json) where T : class
+        {
+            if(json == null || json=="")
+                return null;
+            var formatter = new DataContractJsonSerializer(typeof(T));
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            {
+                var result = formatter.ReadObject(stream) as T;
+                return result;
+            }
+        }
+    }
+}
--- Types/DCTrack.cs
+++ Types/DCTrack.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
 using MapAssist.Settings;
 using System.Threading;
 using MapAssist.Helpers;
+using System.Runtime.Serialization;
 
 namespace MapAssist.Types
 {
@@ -17,6 +17,7 @@ namespace MapAssist.Types
     {
         private List<DC> diabloCloneProgress = new List<DC>();
         private List<DC> show = new List<DC>();
+
         public DCTrack()
         {
             var task = Task.Run(() =>
@@ -25,13 +26,46 @@ namespace MapAssist.Types
                 {
                     try
                     {
+                        var dclist = HttpHelper.GetDC();
+                        dclist = dclist.Replace('\'', '\"');
+                        var dcRaws = Serializer.JsonToObject<List<DCRaw>>(dclist);
+                        diabloCloneProgress.Clear();
+                        foreach (DCRaw dcr in dcRaws)
+                        {
+                            string server;
+                            string ladder;
+                            string sc;
+                            if (dcr.region.Equals("1"))
+                                server = "美服";
+                            else if (dcr.region.Equals("2"))
+                                server = "欧服";
+                            else if (dcr.region.Equals("3"))
+                                server = "亚服";
+                            else
+                                server = "错误";
+
+                            if (dcr.ladder.Equals("2"))
+                                ladder = "经典";
+                            else if (dcr.ladder.Equals("1"))
+                                ladder = "天梯";
+                            else
+                                ladder = "错误";
+ 
+                            if (dcr.hc.Equals("2"))
+                                sc = "普通";
+                            else if (dcr.hc.Equals("1"))
+                                sc = "专家";
+                            else
+                                sc = "错误";
+                            var pro = int.Parse(dcr.progress);
+                            diabloCloneProgress.Add(new DC(server, sc, ladder, pro));
+                        }
+
                         if (MapAssistConfiguration.Loaded.DCTrack.Enabled)
                         {
-                            DCProgress_Refresh();
                             show.Clear();
                             var warningLevel = int.Parse("" + (MapAssistConfiguration.Loaded.DCTrack.WarningLevel.Trim())[0]);
 
-                            //WindowsExternal.Beep(800, 300);
                             if (MapAssistConfiguration.Loaded.DCTrack.Asia)
                             {
                                 if (MapAssistConfiguration.Loaded.DCTrack.Ladder)
@@ -199,91 +233,6 @@ namespace MapAssist.Types
             });
         }
 
-        private string GetWebClient(string url)
-        {
-            var strHTML = "";
-            var myWebClient = new WebClient();
-            myWebClient.Headers.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 6.0; " +
-                                  "Windows NT 6.1; .NET CLR 1.0.3705;)");
-            myWebClient.Proxy = null;
-            Stream myStream = myWebClient.OpenRead(url);
-            var sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
-            strHTML = sr.ReadToEnd();
-            myStream.Close();
-            return strHTML;
-        }
-
-        private void DCProgress_Refresh()
-        {
-
-            var doc = new HtmlAgilityPack.HtmlDocument();
-            var html = GetWebClient("https://diablo2.io/dclonetracker.php");
-
-            var dcp = new List<DC>();
-            doc.LoadHtml(html);
-
-            for (var i = 0; i < 12; i++)
-            {
-                var index = i + 1;
-
-                int pro;
-                string server;
-                string ladder;
-                string sc;
-                string time;
-                HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id=\"memberlist\"]/tbody/tr[" + index.ToString() + "]/td[1]/span[1]/code");
-                Debug.WriteLine(node.InnerText);  //输出节点内容     
-                pro = int.Parse(""+node.InnerText.Trim()[0]);
-
-                HtmlNode node2 = doc.DocumentNode.SelectSingleNode("//*[@id=\"memberlist\"]/tbody/tr[" + index.ToString() + "]/td[2]/span[1]/span");
-                Debug.WriteLine(node2.InnerText);  //输出节点内容    
-                if (node2.InnerText.Contains("Americas"))
-                    server = "美服";
-                else if (node2.InnerText.Contains("Europe"))
-                    server = "欧服";
-                else if (node2.InnerText.Contains("Asia"))
-                    server = "亚服";
-                else
-                    server = "错误";
-
-                HtmlNode node3 = doc.DocumentNode.SelectSingleNode("//*[@id=\"memberlist\"]/tbody/tr[" + index.ToString() + "]/td[3]/span[1]/span");
-                Debug.WriteLine(node3.InnerText);  //输出节点内容    
-                if (node3.InnerText.Contains("Non-Ladder"))
-                    ladder = "经典";
-                else if (node3.InnerText.Contains("Ladder"))
-                    ladder = "天梯";
-                else
-                    ladder = "错误";
-
-                HtmlNode node4 = doc.DocumentNode.SelectSingleNode("//*[@id=\"memberlist\"]/tbody/tr[" + index.ToString() + "]/td[4]/span[1]/span");
-                Debug.WriteLine(node4.InnerText);  //输出节点内容    
-                if (node4.InnerText.Contains("Softcore"))
-                    sc = "普通";
-                else if (node4.InnerText.Contains("Hardcore"))
-                    sc = "专家";
-                else
-                    sc = "错误";
-
-                HtmlNode node5 = doc.DocumentNode.SelectSingleNode("//*[@id=\"memberlist\"]/tbody/tr[" + index.ToString() + "]/td[5]/span[1]/span");
-                Debug.WriteLine(node5.InnerText);  //输出节点内容    
-                time = node5.InnerText;
-                time = time.Replace("days", "天");
-                time = time.Replace("day", "天");
-                time = time.Replace("week", "周");
-                time = time.Replace("weeks", "周");
-                time = time.Replace("minutes", "分钟");
-                time = time.Replace("minute", "分钟");
-                time = time.Replace("hours", "小时");
-                time = time.Replace("hour", "小时");
-                time = time.Replace("ago", "前");
-                time = time.Replace(" ", "");
-                time = time.Trim();
-
-                dcp.Add(new DC(server, sc, ladder, pro));
-            }
-            diabloCloneProgress = dcp;
-        }
-
         public List<DC> DC => show;
     }
 
@@ -308,5 +257,20 @@ namespace MapAssist.Types
         {
             return region+ladder+sc+"模式进度:"+progress.ToString()+"/6";
         }
+    }
+
+    [DataContract]
+    public class DCRaw
+    {
+        [DataMember]
+        public string progress;
+        [DataMember]
+        public string region;
+        [DataMember]
+        public string ladder;
+        [DataMember]
+        public string hc;
+        [DataMember]
+        public string timestamped;
     }
 }
