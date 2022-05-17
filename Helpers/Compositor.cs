25cbe0939fd3348b8cb0974177007e31cadc4abc
elmagnifico <elmagnificogg@gmail.com>
2022-05-17 17:34:47 +0800

    fix small bug that cant chenge the tip about hotkey

--- Helpers/Compositor.cs
+++ Helpers/Compositor.cs
@@ -933,7 +933,7 @@ namespace MapAssist.Helpers
             // Game Name
             if (MapAssistConfiguration.Loaded.GameInfo.ShowGameName && _gameData.Session.GameName.Length > 0)
             {
-                var gameNameText = "房间名: " + _gameData.Session.GameName+" (F8复制)";
+                var gameNameText = "房间名: " + _gameData.Session.GameName+" ("+ MapAssistConfiguration.Loaded.HotkeyConfiguration.ShareRoomKey+ "复制)";
                 DrawText(gfx, anchor, gameNameText, font, fontSize, textColor, true, textAlign);
                 anchor.Y += lineHeight;
 
